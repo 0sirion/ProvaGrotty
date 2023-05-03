4f9772eafe45aa399358b3b723b626082d4eb1a0
0sirion <salvatore.arcuri.93@gmail.com>
2023-05-03 12:56:13 +0200

    exercise

--- app.py
+++ app.py
@@ -5,45 +5,39 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
-import mlem
+import joblib
 import streamlit as st
 
 def main():
-    data = pd.read_csv("https://frenzy86.s3.eu-west-2.amazonaws.com/python/data/formart_house.csv")
-
-    data = data.iloc[:-1 , :]
-    data = data.astype(float)
-    # print(data.describe())
-    ###########################################################################################################
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-
-    st.title('Prova Machine Learning 5 Maggio 2023')
-
-    fig, axs = plt.subplots(ncols=7, nrows=2, figsize=(20, 10))
-    index = 0
-    axs = axs.flatten()
-    for k,v in data.items():
-        sns.distplot(v, ax=axs[index])
-        index += 1
-    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=5.0)
-
-    #####################################################################
-    sns.set(rc={'figure.figsize':(11.7,8.27)})
-    sns.distplot(data['medv'], bins=30)
+    url="https://frenzy86.s3.eu-west-2.amazonaws.com/python/data/formart_house.csv"
+    home=pd.read_csv(url)
+    home=home.iloc[:-1]
+    home=home.apply(pd.to_numeric)
+    
+    fig=plt.figure(figsize=(10,8))
+    plt.title('case')
+    sns.heatmap(home.corr(),annot=True , cmap="Blues")
     st.pyplot(fig)
 
-    #######**********#CORRELATION MATRIX****** ########
-    data.corr()
-    correlation_matrix = data.corr()
-    correlation_matrix
-    sns.heatmap(data=correlation_matrix, annot=True)
+#########################################################
+    crim=st.number_input('inserisci crim',1,10000,500)
+    zn=st.number_input('inserisci zn',1,10000,500)
+    indus=st.number_input('inserisci indus',1,10000,500)
+    chas=st.number_input('inserisci indus',0,10,0)
+    nox=st.number_input('inserisci nox',0.0,1.0,0.5)
+    rm=st.number_input('inserisci rm',1,10,5)
+    age=st.number_input('inserisci age',18,100,30)
+    dis=st.number_input('inserisci dis',1,10,2)
+    rad=st.number_input('inserisci rad',1,3,1)
+    tax=st.number_input('inserisci tax',1,500,100)
+    ptratio=st.number_input('inserisci ptratio',1,50,10)
+    b=st.number_input('inserisci b',1,300,100)
+    
 
-
-
-    fig, ax = plt.subplots()
-    sns.heatmap(data.corr(), ax=ax, annot=True)
-    st.write(fig)
+    lstat=st.number_input('inserisci istat',1,10,5)
+    load_model=joblib.load('modello_home.pkl')
+    pred=load_model.predict([[crim,zn,indus,chas,nox,rm,age,dis,rad,tax,ptratio,b,lstat,]])
+    st.write(f"il prezzo é: euro{round(pred[0],2)}")
 
 if __name__ == '__main__':
     main()
